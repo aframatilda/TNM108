21e4e4d1175b96d9192256e04d739a913e1de007
aframatilda <56309755+aframatilda@users.noreply.github.com>
2021-11-29 15:10:10 +0100

    Attempt 1

--- Project/main.py
+++ Project/main.py
@@ -2,16 +2,34 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-sns.set()
-
+import matplotlib.pyplot as plt
+from yellowbrick.target import JointPlotVisualizer
 
 blues_df = pd.read_csv(
-    "/Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Projekt")
-# rock_df = pd.read_csv("../input/spotify-multigenre-playlists-data/rock_music_data.csv")
-# metal_df = pd.read_csv("../input/spotify-multigenre-playlists-data/metal_music_data.csv")
-# pop_df = pd.read_csv("../input/spotify-multigenre-playlists-data/pop_music_data.csv")
-# indie_df = pd.read_csv("../input/spotify-multigenre-playlists-data/indie_alt_music_data.csv")
-# alt_df = pd.read_csv("../input/spotify-multigenre-playlists-data/alternative_music_data.csv")
-# hiphop_df = pd.read_csv("../input/spotify-multigenre-playlists-data/hiphop_music_data.csv")
+    "/Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Github/TNM108/Project/archive/blues_music_data.csv")
+rock_df = pd.read_csv(
+    "/Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Github/TNM108/Project/archive/rock_music_data.csv")
+metal_df = pd.read_csv(
+    "/Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Github/TNM108/Project/archive/metal_music_data.csv")
+pop_df = pd.read_csv(
+    "/Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Github/TNM108/Project/archive/pop_music_data.csv")
+indie_df = pd.read_csv(
+    "/Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Github/TNM108/Project/archive/indie_alt_music_data.csv")
+# alt_df = pd.read_csv(
+#     "./Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Github/TNM108/Project/archive/alternative_music_data.csv", usecols=col_list)
+hiphop_df = pd.read_csv(
+    "/Users/afra/Desktop/Dokument/TNM108 - Maskininlärning/Github/TNM108/Project/archive/hiphop_music_data.csv")
 
-print(blues_df)
+all_genres = [blues_df, rock_df, metal_df, pop_df, indie_df, hiphop_df]
+feature_names = ["acousticness", "danceability", "energy",
+                 "instrumental", "liveness", "loudness", "tempo", "valence"]
+
+X, y = all_genres[feature_names], all_genres["genre"]
+
+features = np.array(feature_names)
+
+visualizer = FeatureCorrelation(labels=features)
+
+plt.rcParams["figure.figsize"] = (20, 20)
+visualizer.fit(X, y)
+visualizer.show()
